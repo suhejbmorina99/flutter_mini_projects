033287dcf235b4898f1e467937e1535171598e7a
Suhejb <suhejbmorina@gmail.com>
2024-02-07 22:15:00 +0100

    learning custom widget

--- rolle_dice/lib/main.dart
+++ rolle_dice/lib/main.dart
@@ -4,23 +4,32 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 74, 58, 223),
-                Color.fromARGB(255, 139, 127, 240)
-              ],
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(color: Colors.white, fontSize: 18),
-            ),
-          ),
-        ),
+        body: BodyContent(),
       ),
     ),
   );
+}
+
+class BodyContent extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 74, 58, 223),
+            Color.fromARGB(255, 139, 127, 240)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(color: Colors.white, fontSize: 18),
+        ),
+      ),
+    );
+  }
 }
