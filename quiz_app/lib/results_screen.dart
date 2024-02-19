b575961f0c2918018b7d5a34fa6652b9ac65f586
Suhejb <suhejbmorina@gmail.com>
2024-02-19 23:40:23 +0100

    style changing

--- quiz_app/lib/results_screen.dart
+++ quiz_app/lib/results_screen.dart
@@ -51,7 +51,7 @@ class ResultsScreen extends StatelessWidget {
             Text(
               'You answered $numCorrectQuestions out of $numTotalQuestions questions correctly!',
               style: GoogleFonts.lato(
-                color: const Color.fromARGB(255, 230, 200, 253),
+                color: Colors.white,
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),
