1a7f0a02d989c39d775900bd7a6facd192f09759
Suhejb <suhejbmorina@gmail.com>
2024-02-28 23:19:57 +0100

    learned how to put a overlay and add a text field

---
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() {
+    return _NewEXpenseState();
+  }
+}
+
+class _NewEXpenseState extends State<NewExpense> {
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            maxLength: 50,
+            decoration: InputDecoration(
+              label: Text('Title'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
