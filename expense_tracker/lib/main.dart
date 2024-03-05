0388d0a27c8b51c74b8c1ba84f5814785500e898
Suhejb <suhejbmorina@gmail.com>
2024-03-05 22:26:46 +0100

    understanding the themeData properties and colors

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 255, 0, 0),
+);
+
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
+      home: const Expenses(),
     ),
   );
 }
