e1e403ebd033e347eb7e3cc4ea327ac4396dbf25
suhejb <suhejbmorina@gmail.com>
2024-03-06 15:07:50 +0100

    added dark theme, depend on system theme

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -5,10 +5,29 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 157, 211, 113),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.all(6),
+        ),
+        // filledButtonTheme: FilledButtonThemeData(
+        //   style: FilledButton.styleFrom(
+        //     backgroundColor: kDarkColorScheme.primaryContainer,
+        //     foregroundColor: kDarkColorScheme.onPrimaryContainer,
+        //   ),
+        // ),
+      ),
       theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
           backgroundColor: kColorScheme.onPrimaryContainer,
           foregroundColor: kColorScheme.primaryContainer,
@@ -25,10 +44,11 @@ void main() {
         textTheme: ThemeData().textTheme.copyWith(
               titleLarge: const TextStyle(
                 fontWeight: FontWeight.normal,
-                color: Colors.black, // Nese qeky text theme perdoret dika tjeter
+                color:
+                    Colors.black, // Nese qeky text theme perdoret dika tjeter
                 // psh ne expense item i merr qeto atribute por atributi color
                 // i shkon vetem disa elementeve jo krejt texteve sepse
-                // kemi perdorur foregroundColor psh per appBartheme
+                // kemi perdorur foregroundColor psh per
                 fontSize: 16,
               ),
             ),
