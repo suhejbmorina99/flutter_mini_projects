3ff101aca108d2702dd57a28c33ecd86bc94b2b3
suhejb <suhejbmorina@gmail.com>
2024-03-11 10:11:18 +0100

    disable landscape orientation, use only portraitUp

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 157, 211, 113),
@@ -11,49 +12,57 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  runApp(
-    MaterialApp(
-      darkTheme: ThemeData.dark().copyWith(
-        colorScheme: kDarkColorScheme,
-        cardTheme: const CardTheme().copyWith(
-          color: kDarkColorScheme.secondaryContainer,
-          margin: const EdgeInsets.all(6),
-        ),
-        // filledButtonTheme: FilledButtonThemeData(
-        //   style: FilledButton.styleFrom(
-        //     backgroundColor: kDarkColorScheme.primaryContainer,
-        //     foregroundColor: kDarkColorScheme.onPrimaryContainer,
-        //   ),
-        // ),
-      ),
-      theme: ThemeData().copyWith(
-        colorScheme: kColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer,
-        ),
-        cardTheme: const CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
-          margin: const EdgeInsets.all(6),
-        ),
-        filledButtonTheme: FilledButtonThemeData(
-          style: FilledButton.styleFrom(
-            backgroundColor: Colors.black,
+  // Me WidgetsFlutterBinding.ensureInitialized();
+  // sigurohemi qe ka mu initicalizu kodi me posht
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([ // Me qete systemChrome e bojm lock ose disable orientations
+    //te tjera edhe e vendosim cilem dojm me lon si default.
+    DeviceOrientation.portraitUp,
+  ]).then((fn) {
+    runApp(
+      MaterialApp(
+        darkTheme: ThemeData.dark().copyWith(
+          colorScheme: kDarkColorScheme,
+          cardTheme: const CardTheme().copyWith(
+            color: kDarkColorScheme.secondaryContainer,
+            margin: const EdgeInsets.all(6),
           ),
+          // filledButtonTheme: FilledButtonThemeData(
+          //   style: FilledButton.styleFrom(
+          //     backgroundColor: kDarkColorScheme.primaryContainer,
+          //     foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          //   ),
+          // ),
         ),
-        textTheme: ThemeData().textTheme.copyWith(
-              titleLarge: const TextStyle(
-                fontWeight: FontWeight.normal,
-                color:
-                    Colors.black, // Nese qeky text theme perdoret dika tjeter
-                // psh ne expense item i merr qeto atribute por atributi color
-                // i shkon vetem disa elementeve jo krejt texteve sepse
-                // kemi perdorur foregroundColor psh per
-                fontSize: 16,
-              ),
+        theme: ThemeData().copyWith(
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          ),
+          cardTheme: const CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: const EdgeInsets.all(6),
+          ),
+          filledButtonTheme: FilledButtonThemeData(
+            style: FilledButton.styleFrom(
+              backgroundColor: Colors.black,
             ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: const TextStyle(
+                  fontWeight: FontWeight.normal,
+                  color:
+                      Colors.black, // Nese qeky text theme perdoret dika tjeter
+                  // psh ne expense item i merr qeto atribute por atributi color
+                  // i shkon vetem disa elementeve jo krejt texteve sepse
+                  // kemi perdorur foregroundColor psh per
+                  fontSize: 16,
+                ),
+              ),
+        ),
+        home: const Expenses(),
       ),
-      home: const Expenses(),
-    ),
-  );
+    );
+  });
 }
