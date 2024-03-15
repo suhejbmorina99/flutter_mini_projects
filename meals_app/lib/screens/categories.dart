61aa22763f1e39247c76761ad9eec1c7bbf39fd1
suhejb <suhejbmorina@gmail.com>
2024-03-15 16:08:13 +0100

    added the navigator to another screen and populate the selected category with correct data

--- meals_app/lib/screens/categories.dart
+++ meals_app/lib/screens/categories.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
+import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class Categories extends StatelessWidget {
   const Categories({super.key});
+
+  void _selectedCategory(BuildContext context, Category category) {
+    final selectedCategory = dummyMeals
+        .where(
+          (meal) => meal.categories.contains(category.id),
+        )
+        .toList();
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          categoryName: category.title,
+          meals: selectedCategory,
+        ),
+      ),
+    );
+    // Kjo mundet mu bo edhe ne formen me posht,
+    // por perdoret ajo lart me shume.
+    // Navigator.push(context, route)
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +47,11 @@ class Categories extends StatelessWidget {
           //     .toList(),
 
           for (final specificCategory in availableCategories)
-            CategoryGridItem(category: specificCategory),
+            CategoryGridItem(
+              category: specificCategory,
+              onSelectedCategory: () =>
+                  _selectedCategory(context, specificCategory),
+            ),
         ],
       ),
     );
--- meals_app/lib/widgets/category_grid_item.dart
+++ meals_app/lib/widgets/category_grid_item.dart
@@ -3,16 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
-  const CategoryGridItem({super.key, required this.category});
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+    required this.onSelectedCategory,
+  });
 
   final Category category;
+  final void Function() onSelectedCategory;
 
   @override
   Widget build(BuildContext context) {
     // Me InkWell e bojm cdo element tapable
     return InkWell(
-      onTap: () {},
-      borderRadius: BorderRadius.circular(10), // Ky borderRadius per masi qe ta bon tap
+      onTap: onSelectedCategory,
+      borderRadius:
+          BorderRadius.circular(10), // Ky borderRadius per masi qe ta bon tap
       splashColor: Colors.black,
       child: Container(
         padding: const EdgeInsets.all(16),
@@ -25,7 +31,8 @@ class CategoryGridItem extends StatelessWidget {
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
           ),
-          borderRadius: BorderRadius.circular(10), // Ky borderRadius per deafult elementet
+          borderRadius: BorderRadius.circular(
+              10), // Ky borderRadius per deafult elementet
         ),
         child: Text(
           category.title,
