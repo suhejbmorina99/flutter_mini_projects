c6c52225741dfe7755b124b654a8277fd0429deb
suhejb <suhejbmorina@gmail.com>
2024-04-02 16:05:56 +0200

    working on the home screen

--- go_questions/lib/screens/home_screen.dart
+++ go_questions/lib/screens/home_screen.dart
@@ -10,10 +10,53 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(
           'Str8ch',
-          style: GoogleFonts.nunito(),
+          style: GoogleFonts.nunito(
+            fontSize: 24,
+          ),
         ),
         centerTitle: true,
         backgroundColor: Colors.white,
+      ),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Embrace Movement, Embrace Freedom.',
+              style: GoogleFonts.nunito(
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(
+              height: 70,
+            ),
+            Image.asset(
+              'assets/images/home_image.png',
+              width: 350,
+            ),
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(horizontal: 80),
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.lightBlue,
+              ),
+              child: Text(
+                'Continue',
+                style: GoogleFonts.nunito(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+          ],
+        ),
       ),
     );
   }
