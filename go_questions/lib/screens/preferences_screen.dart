2c483b940f2c16f052bc67ebea6f1a117b4e949f
Suhejb <suhejbmorina@gmail.com>
2024-04-26 15:10:54 +0200

    now the bottom bar navigation it's working

--- go_questions/lib/screens/preferences_screen.dart
+++ go_questions/lib/screens/preferences_screen.dart
@@ -5,6 +5,11 @@ class PreferencesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: Center(
+        child: Text('Preferences Screen'),
+      ),
+    );
   }
 }
--- go_questions/lib/screens/workouts.dart
+++ go_questions/lib/screens/workouts.dart
@@ -1,13 +1,62 @@
 import 'package:curved_labeled_navigation_bar/curved_navigation_bar.dart';
 import 'package:curved_labeled_navigation_bar/curved_navigation_bar_item.dart';
 import 'package:flutter/material.dart';
+import 'package:go_questions/screens/preferences_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class WorkoutScreen extends StatelessWidget {
+class WorkoutScreen extends StatefulWidget {
   const WorkoutScreen({super.key});
 
   @override
+  State<WorkoutScreen> createState() {
+    return _WorkoutScreenState();
+  }
+}
+
+class _WorkoutScreenState extends State<WorkoutScreen> {
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    Widget activeScreen = Column(
+      children: [
+        Expanded(
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.red,
+                  child: const Center(child: Text('First Row')),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.blueAccent,
+                  child: const Center(child: Text('Second Row')),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+
+    if (_selectedPageIndex == 1) {
+      activeScreen = const PreferencesScreen();
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -19,35 +68,9 @@ class WorkoutScreen extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.white,
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    color: Colors.red,
-                    child: const Center(child: Text('First Row')),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    color: Colors.blueAccent,
-                    child: const Center(child: Text('Second Row')),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+      body: activeScreen,
       bottomNavigationBar: CurvedNavigationBar(
+        onTap: _selectPage,
         items: const [
           CurvedNavigationBarItem(
             child: Icon(Icons.sports_gymnastics),
